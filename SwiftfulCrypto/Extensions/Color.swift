1f18d54747f7c7af02b93c9bab34899f77a3ec0b
Chathura Hettiarachchi <chathura93@yahoo.com>
2024-04-08 06:26:44 +1200

    initial project setup

---
+++ SwiftfulCrypto/Extensions/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  SwiftfulCrypto
+//
+//  Created by Chathura Hetti Arachchi on 08/04/2024.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Chathura Hetti Arachchi on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
